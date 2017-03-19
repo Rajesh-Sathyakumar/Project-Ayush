23aa109ede36edb75bfae64f317c0b92ea75d5ef
yuva4297 <yuvaammu3@gmail.com>
2017-03-20 00:20:41 +0530

    drugs
    drugs, drugs_details are added [YR- 03/20/2017]

--- Modules/DB-Design Scripts/ayush.sql
+++ Modules/DB-Design Scripts/ayush.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 19, 2017 at 12:38 PM
+-- Generation Time: Mar 19, 2017 at 07:34 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.1.1
 
