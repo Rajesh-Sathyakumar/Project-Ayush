a62e75ca5d66a15b95645d9ea9dd05903dacfd8e
swarna97 <swarna.ilas@gmail.com>
2017-08-01 22:41:49 +0530

    Update index.php

--- final/final user/Modules/homepage/index.php
+++ final/final user/Modules/homepage/index.php
@@ -17,54 +17,39 @@ if(isset($_POST["submit"]))
 
 	<meta charset="utf-8">
 
-	<!-- Template site title
-   ================================================== -->
 	<title>Ministry of Ayush</title>
 
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="keywords" content="">
 	<meta name="description" content="">
-<!--
 
-Template 2078 Adventure
 
-http://www.tooplate.com/view/2078-adventure
-
--->
-	<!-- Bootstrap CSS
-   ================================================== -->
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 
-	<!-- Animate CSS
-   ================================================== -->
+
 	<link rel="stylesheet" href="css/animate.min.css">
 
-	<!-- Font Icons
-   ================================================== -->
+	
 	<link rel="stylesheet" href="css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/et-line-font.css">
 
-	<!-- Nivo Lightbox CSS
-   ================================================== -->
+
+
 	<link rel="stylesheet" href="css/nivo-lightbox.css">
 	<link rel="stylesheet" href="css/nivo_themes/default/default.css">
 
-	<!-- Owl Carousel CSS
-   ================================================== -->
+	
    	<link rel="stylesheet" href="css/owl.theme.css">
 	<link rel="stylesheet" href="css/owl.carousel.css">
 
-	<!-- BxSlider CSS
-   ================================================== -->
+	
    	<link rel="stylesheet" href="css/bxslider.css">
 
-   	<!-- Main CSS
-   	================================================== -->
+   	
 	<link rel="stylesheet" href="css/style.css">
 
-	<!-- Google web font
-   ================================================== -->
+	
 	<link href='https://fonts.googleapis.com/css?family=Raleway:700' rel='stylesheet' type='text/css'>
 	
 </head>
